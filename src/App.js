ae208069870091c97cac3e54a983f38d67d9d4ce
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-11-22 14:10:56 +0500

    Set routes for app navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Teams from './components/Teams/Teams';
+import Players from './components/Players/Players';
+import Standings from './components/Standings/Standings';
+import TopScores from './components/Top Scores/TopScores';
+import Trophies from './components/Trophies/Trophies';
+
 function App() {
   return (
-    <div />
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Teams />} />
+        <Route path="players" element={<Players />} />
+        <Route path="standings" element={<Standings />} />
+        <Route path="topscores" element={<TopScores />} />
+        <Route path="trophies" element={<Trophies />} />
+      </Routes>
+    </>
   );
 }
 
