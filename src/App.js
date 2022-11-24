7c909c6366bb75e552d2985f96b08d11effa5da5
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-11-24 14:19:42 +0500

    Update styling of the app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import Navbar from './components/Navbar';
+// import Header from './components/Header';
 import CryptoList from './Pages/CoinsList';
 import fetchDataFromApi from './redux/AllCoins/thunk';
+import CoinDetails from './Pages/CoinDetails';
 
 function App() {
   const dispatch = useDispatch();
@@ -18,6 +20,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<CryptoList />} />
+        <Route path="/details" element={<CoinDetails />} />
       </Routes>
     </>
   );
