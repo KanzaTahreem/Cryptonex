20b6dcbb487ac588be36cf303166717827c9a455
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-11-25 00:02:41 +0500

    Update redux store with chartReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import coinsReducer from './AllCoins/coins';
+import chartReducer from './CoinChart/chart';
 
 const store = configureStore({
   reducer: {
     crypto: coinsReducer,
+    chart: chartReducer,
   },
 });
 
