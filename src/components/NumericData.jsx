6b9db2f5557e11367b365502b0a1b0fc9414289b
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-11-26 00:48:52 +0500

    Add coin statistics to the UI

--- src/components/NumericData.jsx
+++ src/components/NumericData.jsx
@@ -1,96 +1,150 @@
+import millify from 'millify';
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import {
+  BsGraphUp, BsGraphDown, BsCheck2All, BsHash,
+} from 'react-icons/bs';
+import { ImStatsBars } from 'react-icons/im';
+import { HiArrowNarrowUp, HiArrowNarrowDown } from 'react-icons/hi';
+import { GiProgression } from 'react-icons/gi';
+import { RiExchangeFundsFill } from 'react-icons/ri';
+import { BiCoinStack, BiDollarCircle } from 'react-icons/bi';
+import '../styles/CoinDetails.css';
 
 const NumericData = () => {
   const location = useLocation();
   const { coin } = location.state;
+  // const countColor = count > 0 ? 'green' : 'red';
 
   return (
-    <>
-      <div>{coin.id}</div>
-      <div>
-        <img src={coin.image} alt={`${coin.image}-logo`} />
-        <p>
-          {coin.name}
-          /
-          {coin.symbol}
-        </p>
-      </div>
-      <div>
-        <p>{`$${coin.current_price}`}</p>
-        <p>
-          market-cap:
-          {' '}
-          {coin.market_cap}
-        </p>
-        <p>
-          market cap change:
-          {' '}
-          {coin.market_cap_change_24h}
-        </p>
-        <p>
-          market change %:
-          {' '}
-          {coin.market_cap_change_percentage}
-        </p>
-        <p>
-          rank:
-          {' '}
-          {coin.market_cap_rank}
-        </p>
-        <p>
-          dilutated value:
-          {' '}
-          {coin.fully_diluted_valuation}
-        </p>
+    <div className="coin-details">
+      <div className="coin">
+        <div>
+          <p className="name">
+            {coin.name}
+            {' '}
+            {`(${coin.symbol.toUpperCase()})`}
+          </p>
+          <img src={coin.image} alt={`${coin.image}-logo`} />
+        </div>
 
-        <p>
-          high 24h:
-          {' '}
-          {coin.high_24h}
-        </p>
-        <p>
-          low 24h:
-          {' '}
-          {coin.low_24h}
-        </p>
-        <p>
-          price change 24h:
-          {' '}
-          {coin.price_change_24h}
-        </p>
-        <p>
-          price change %:
-          {' '}
-          {coin.price_change_percentage_24h}
-        </p>
-        <p>
-          total supply:
-          {' '}
-          {coin.total_supply}
-        </p>
-        <p>
-          max supply:
-          {' '}
-          {coin.max_supply}
-        </p>
-        <p>
-          ath:
-          {' '}
-          {coin.ath}
-        </p>
-        <p>
-          ath change %:
-          {' '}
-          {coin.ath_change_percentage}
-        </p>
-        <p>
-          last updated:
-          {' '}
-          {coin.last_updated}
-        </p>
+        <div>
+          <div className="price">
+            <p>
+              <span><BiDollarCircle /></span>
+              <span>Price</span>
+            </p>
+            <p>{`$ ${millify(coin.current_price)}`}</p>
+          </div>
+
+          <div className="rank">
+            <p>
+              <span><BsHash /></span>
+              <span>Rank</span>
+            </p>
+            <p>{coin.market_cap_rank}</p>
+          </div>
+        </div>
       </div>
-    </>
+
+      <h2 className="stats-title">
+        {coin.name}
+        {' '}
+        {`(${coin.symbol.toUpperCase()})`}
+        {' '}
+        Statistics
+      </h2>
+
+      <div className="coin-stats">
+        <div>
+          <p className="stats-name">
+            <span><GiProgression /></span>
+            <span> Market Cap</span>
+          </p>
+          <p className="value">{`$ ${millify(coin.market_cap)}`}</p>
+        </div>
+
+        <div>
+          <p className="stats-name">
+            <span><RiExchangeFundsFill /></span>
+            <span>Market Cap Change</span>
+          </p>
+          <p className="value">
+            {`$ ${millify(coin.market_cap_change_24h)}`}
+            {' / '}
+            {`${millify(coin.market_cap_change_percentage_24h)} %`}
+          </p>
+        </div>
+
+        <div>
+          <p className="stats-name">
+            <span><BiCoinStack /></span>
+            <span>Dilutated Value</span>
+          </p>
+          <p className="value">{`$ ${millify(coin.fully_diluted_valuation)}`}</p>
+        </div>
+
+        <div>
+          <p className="stats-name">
+            <span><BsCheck2All /></span>
+            <span>Total Supply</span>
+          </p>
+          <p className="value">{`$ ${millify(coin.total_supply)}`}</p>
+        </div>
+
+        <div>
+          <p className="stats-name">
+            <span><HiArrowNarrowUp /></span>
+            <span>24h High</span>
+          </p>
+          <p className="value">{`$ ${millify(coin.high_24h)}`}</p>
+        </div>
+
+        <div>
+          <p className="stats-name">
+            <span><HiArrowNarrowDown /></span>
+            <span>24h Low</span>
+          </p>
+          <p className="value">{`$ ${millify(coin.low_24h)}`}</p>
+        </div>
+
+        <div>
+          <p className="stats-name">
+            <span><ImStatsBars /></span>
+            <span>24h Price Change</span>
+          </p>
+          <p className="value">
+            {`$ ${millify(coin.price_change_24h)}`}
+            {' / '}
+            {`${millify(coin.price_change_percentage_24h)} %`}
+          </p>
+        </div>
+
+        <div>
+          <p className="stats-name">
+            <span><BsGraphUp /></span>
+            <span>All Time High</span>
+          </p>
+          <p className="value">
+            {`$ ${millify(coin.ath)}`}
+            {' / '}
+            {`${millify(coin.ath_change_percentage)} %`}
+          </p>
+        </div>
+
+        <div>
+          <p className="stats-name">
+            <span><BsGraphDown /></span>
+            <span>All Time Low</span>
+          </p>
+          <p className="value">
+            {`$ ${millify(coin.atl)}`}
+            {' / '}
+            {`${millify(coin.atl_change_percentage)} %`}
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
