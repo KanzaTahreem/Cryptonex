6370a952edc5345c49068c07d202420ec427ce4d
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-11-25 12:29:11 +0500

    Add back button to navigate back to homepage

--- src/Pages/CoinDetails.jsx
+++ src/Pages/CoinDetails.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { IoIosArrowBack } from 'react-icons/io';
 import CoinChart from '../components/CoinChart';
 import NumericData from '../components/NumericData';
 import fetchChartFromApi from '../redux/CoinChart/thunk';
@@ -10,13 +11,19 @@ const CoinDetails = () => {
   const dispatch = useDispatch();
 
   const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(fetchChartFromApi(location.state.coin.id));
   }, []);
 
+  const handleBackBtn = () => navigate(-1);
+
   return (
     <section className="coin-deatils-container">
+      <button type="button" onClick={handleBackBtn}>
+        <IoIosArrowBack />
+      </button>
       <NumericData />
       <CoinChart />
     </section>
