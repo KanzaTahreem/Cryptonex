c6aa1ef41dd8050a8c33859a38dfef8309c225f4
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-12-05 16:14:52 +0500

    Update redux store with globalDataReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import coinsReducer from './AllCoins/coins';
 import chartReducer from './CoinChart/chart';
+import globalDataReducer from './Global/global';
 
 const store = configureStore({
   reducer: {
     crypto: coinsReducer,
     chart: chartReducer,
+    global: globalDataReducer,
   },
 });
 
