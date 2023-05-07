949c66e337d08d016bd2b91aabf43fbb033cd729
KanzaTahreem <tehreemmsk411@gmail.com>
2023-05-07 06:15:44 +0500

    Update store with trending data reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import coinsReducer from './AllCoins/coins';
 import chartReducer from './CoinChart/chart';
 import globalDataReducer from './Global/global';
+import trendingCurrencyReducer from './Trending/trending';
 
 const store = configureStore({
   reducer: {
     crypto: coinsReducer,
     chart: chartReducer,
     global: globalDataReducer,
+    trending: trendingCurrencyReducer,
   },
 });
 
